b51a6ef2a276ada862aa161dc1b5b43e16bfbb4d
Estelle Wan <eccheung@amazon.com>
2018-04-28 19:08:44 -0700

    Following the leader using coroutines

--- common/communication.py
+++ common/communication.py
@@ -6,8 +6,18 @@ import uuid
 DB_UUID = ""
 WS_CONFIG = ""
 
-async def send(key, value, request_id):
-    async with websockets.connect(WS_CONFIG) as websocket:
+def connect(ws, uuid):
+    global DB_UUID
+    DB_UUID = uuid
+    global WS_CONFIG
+    WS_CONFIG = ws
+
+@asyncio.coroutine
+async def create_send(future, key, value, request_id, ws=None):
+    if ws is None:
+        ws = WS_CONFIG
+
+    async with websockets.connect(ws) as websocket:
         request = {
           "bzn-api": "crud",
           "cmd": "create",
@@ -23,14 +33,27 @@ async def send(key, value, request_id):
         response_string = await websocket.recv()
         response = json.loads(response_string)
 
-        print (response)
+        future.set_result(response)
 
-def connect(ws, uuid):
-    global DB_UUID
-    DB_UUID = uuid
-    global WS_CONFIG
-    WS_CONFIG = ws
+def create_find_leader(key, value, request_id, leader_info=None):
+    ws = WS_CONFIG
+    if leader_info != None:
+        ws_host = leader_info['leader-host']
+        ws_port = leader_info['leader-port']
+        ws =  "ws://" + ws_host + ":" + str(ws_port)
+
+    loop = asyncio.get_event_loop()
+    future = asyncio.Future()
+    asyncio.ensure_future(create_send(future, key, value, request_id, ws))
+    loop.run_until_complete(future)
+    response = future.result()
+
+    if 'error' in response and response['error'] == 'NOT_THE_LEADER':
+        create_find_leader(key, value, request_id, response['data'])
+    else:
+        print(response)
 
 def create(key, value):
     request_id = str(uuid.uuid4())
-    asyncio.get_event_loop().run_until_complete(send(key, value, request_id))
+
+    create_find_leader(key, value, request_id)
