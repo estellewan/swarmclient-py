7502d8b3ec8de867643da1123e853dd196414906
Estelle Wan <eccheung@amazon.com>
2018-04-28 19:24:41 -0700

    Read operation support

--- common/communication.py
+++ common/communication.py
@@ -13,7 +13,7 @@ def connect(ws, uuid):
     WS_CONFIG = ws
 
 @asyncio.coroutine
-async def create_send(future, key, value, request_id, ws=None):
+async def __create_send(future, key, value, request_id, ws=None):
     if ws is None:
         ws = WS_CONFIG
 
@@ -35,7 +35,7 @@ async def create_send(future, key, value, request_id, ws=None):
 
         future.set_result(response)
 
-def create_find_leader(key, value, request_id, leader_info=None):
+def __create(key, value, request_id, leader_info=None):
     ws = WS_CONFIG
     if leader_info != None:
         ws_host = leader_info['leader-host']
@@ -44,16 +44,64 @@ def create_find_leader(key, value, request_id, leader_info=None):
 
     loop = asyncio.get_event_loop()
     future = asyncio.Future()
-    asyncio.ensure_future(create_send(future, key, value, request_id, ws))
+    asyncio.ensure_future(__create_send(future, key, value, request_id, ws))
     loop.run_until_complete(future)
     response = future.result()
 
     if 'error' in response and response['error'] == 'NOT_THE_LEADER':
-        create_find_leader(key, value, request_id, response['data'])
+        __create(key, value, request_id, response['data'])
     else:
         print(response)
 
 def create(key, value):
     request_id = str(uuid.uuid4())
 
-    create_find_leader(key, value, request_id)
+    __create(key, value, request_id)
+
+async def __read_send(future, key, request_id, ws=None):
+    if ws is None:
+        ws = WS_CONFIG
+
+    async with websockets.connect(ws) as websocket:
+        request = {
+          "bzn-api": "crud",
+          "cmd": "read",
+          "data": {
+              "key": key
+          },
+          "db-uuid": DB_UUID,
+          "request-id": request_id
+        }
+        await websocket.send(json.dumps(request))
+
+        response_string = await websocket.recv()
+        response = json.loads(response_string)
+
+        future.set_result(response)
+
+def __read(key, request_id, leader_info=None):
+    ws = WS_CONFIG
+    if leader_info != None:
+        ws_host = leader_info['leader-host']
+        ws_port = leader_info['leader-port']
+        ws =  "ws://" + ws_host + ":" + str(ws_port)
+
+    loop = asyncio.get_event_loop()
+    future = asyncio.Future()
+    asyncio.ensure_future(__read_send(future, key, request_id, ws))
+    loop.run_until_complete(future)
+    response = future.result()
+
+    if 'error' in response and response['error'] == 'NOT_THE_LEADER':
+        __read(key, request_id, response['data'])
+    else:
+        print(response)
+
+def read(key):
+    request_id = str(uuid.uuid4())
+
+    __read(key, request_id)
+
+connect('ws://13.78.131.94:51011', '80174b53-2dda-49f1-9d6a-6a780d4cceee')
+create("hello", 4324234)
+read("hello")
